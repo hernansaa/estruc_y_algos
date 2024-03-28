6a5d1b6e8efc9d168e37719ffc1711e689f386cb
hernansaa <hernansaa88@gmail.com>
2024-03-28 14:00:50 +0100

    eje 5 terminado

--- modulo_4/eje5.c
+++ modulo_4/eje5.c
@@ -5,17 +5,32 @@ empiezan y terminan con la misma letra. */
 
 int main(){
 
-  int c, aux;
+  int c, primera, ultima, cont=0;
 
   printf("Ingresar un texto terminando con un punto:\n");
   c = getchar();
 
-  while (c == '.'){
+  while (c != '.'){
 
-    while 
+    primera = c;
 
+    while (c != ' ' && c != '.'){
+      ultima = c;
+      c = getchar();
+    }
 
+    if (primera == ultima)
+      cont++;
+
+    primera = ' ';
+    ultima = ' ';
+
+    if (c == ' ')
+      c = getchar();
 
   }
 
+  printf("Cantidad de palabras que empiezas y terminan con misma letra: %d\n", cont);
+
+  return 0;
 }
