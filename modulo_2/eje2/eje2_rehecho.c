1d293f544c77a97b22593e37ffdb90ebb4fa3584
hernansaa <hernansaa88@gmail.com>
2026-02-08 01:46:19 +0100

    Fix sorting logic in eje2_rehecho.c

--- modulo_2/eje2/eje2_rehecho.c
+++ modulo_2/eje2/eje2_rehecho.c
@@ -6,7 +6,8 @@ números negativos. Mostrarlo.*/
 #include <stdio.h>
 
 void cargar(int vec[]);
-
+void mostrar(int vec[], int n);
+void mostrarMayores(int vec[], int n);
 
 int main() {
     
@@ -22,8 +23,12 @@ void mostrarMayores(int vec[], int n) {
     int i, j, aux;
     
     for (i=0; i<n-1; i++) {
-        for (j=i+1; j<n; j++){
-            if vec[i] < vec[j];
+        for (j=i+1; j<n; j++) {
+            if (vec[i] < vec[j]) {
+                aux = vec[i];
+                vec[i] = vec[j];
+                vec[j] = aux;
+            }
             
         }
     }
