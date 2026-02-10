860cddab6a54fd3167baded3a4350f517f39d147
hernansaa <hernansaa88@gmail.com>
2026-02-10 15:01:05 +0100

    Improve word counting and printing logic
    Refactor word counting logic to store words in an array and print only if more than one letter.

--- modulo_4/eje2_repaso_final.c
+++ modulo_4/eje2_repaso_final.c
@@ -7,6 +7,7 @@ blanco y eliminando las palabras de 1 letra.*/
 int main() {
     
     int c, contl=0;
+    char aux[100];
     
     printf("Ingrese un texto terminado en un punto.\n");
     c = getchar();
@@ -14,20 +15,32 @@ int main() {
     while (c != '.') {
         
         while (c != '.' && c != ' ') {
-            
+            // putchar(c); // Lo saque para que no imprima, y que imprima desde el aux.
+            aux[contl] = c; 
             contl++;
-            putchar(c);
             c = getchar();
         }    
         
-        printf("(%d) ", contl); // Agrego cantidad de letras, solo para DEBUG.
+        // Ahora imprimimos solo si hay mas de 1 letra en la palabra.
+        if (contl > 1) {
+            printf(" %s", aux);
+        }
         
+        // Si c es un blanco, que siga pasando hasta que no lo sea (para evitar que cuente blancos como palabras.)
         while (c == ' ') {
             c = getchar();
         }
         
-        
+        // Resetea el contador de letras para quedar listo para contar las letras de la siguiente palabra.
         contl=0;
+        
+        // Resetear el aux para que cargue la siguiente palabra sin basaura.
+        for (int i=0; i<100; i++) {
+            aux[i] = 0;
+        }
+    
+        
     }
     
+    return 0;
 }
